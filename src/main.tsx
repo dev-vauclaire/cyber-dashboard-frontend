a20e244f2fe414ce6cff8119ba334aeaacb4c9d2
Dev Vauclaire <dev@ch-vauclaire.fr>
2026-06-21 21:09:24 +0200

    adding the app in the main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.tsx';
-import { SourceColorProvider } from './internals/source-colors/SourceColorContext';
-
-const queryClient = new QueryClient();
+import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <SourceColorProvider>
-        <App />
-      </SourceColorProvider>
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
